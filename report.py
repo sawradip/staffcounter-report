7cacf6a96190d21977f3f28297639a629bcfc1bd
sawradip <sawradip0@gmail.com>
2023-04-16 17:43:52 +0600

    Updated Currency

--- report.py
+++ report.py
@@ -3,15 +3,20 @@ import datetime
 from bs4 import BeautifulSoup
 
 # 0 for current week, -1 for previous week, -2 forweek brfore that etc.
-n_week = 0        
+n_week = 0
+
+full_time = 160  #in hours
+full_time_pay = 50000 
+BDT_to_USD = 106
+
 # Put your staffcounter email id here. 
 EMAIL_ID = "sawradipsaha5@gmail.com"    
 # URL_format = "https://data.staffcounter.net/report/sawradipsaha5@gmail.com?date=2022-12-17"
-
+# https://data.staffcounter.net/report/sawradipsaha5@gmail.com?reloaded=1&date=2023-04-10
 def getDuration(emailId = EMAIL_ID, dateString = "2022-12-17"):
 
     URL = f"https://data.staffcounter.net/report/{emailId}"
-    PARAMS = {"date": dateString}
+    PARAMS = {"reloaded":1, "date": dateString}
     r = requests.get(URL, params=PARAMS)
 
     soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install ' or install html5lib
@@ -42,7 +47,7 @@ startDate = todayDate + datetime.timedelta(days=-todayDate.weekday(), weeks=n_we
 if n_week == 0:
     endDate = todayDate
 else:
-    endDate = startDate + datetime.timedelta(days= 7)
+    endDate = startDate + datetime.timedelta(days= 6)
 
 def getTotalTime(startDate, endDate):
     totalTime = datetime.timedelta(0)
@@ -60,11 +65,17 @@ print( f"""
 Time Report:
 -----------------------""")
 totalTime =  getTotalTime(startDate, endDate)
+total_active_time_str = f"{int(totalTime.total_seconds()//3600)}h {int((totalTime.total_seconds()%3600)//60)}m"
+total_active_time_decimal = int(totalTime.total_seconds()//3600) +  ((totalTime.total_seconds()%3600)//60)/60
 
 print( f"""--------------------------
-start_date: {startDate}
-end_date: {endDate}
-----------------------
-Total Active Time:{int(totalTime.total_seconds()//3600)}h {int((totalTime.total_seconds()%3600)//60)}m
+
+Start_date: {startDate}
+End_date: {endDate}
+Total Active Time: {total_active_time_str}
+BDT : {total_active_time_decimal * full_time_pay / full_time}
+USD: { total_active_time_decimal * full_time_pay / full_time / BDT_to_USD}
+
+
 
  """)
